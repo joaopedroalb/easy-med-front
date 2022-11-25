1568f04b47213bbbb1867bc81af0f22ce47b8c20
Gabriel Porto <gabrieljoserporto@gmail.com>
2022-11-24 22:52:34 -0300

    Segundo commit

--- src/pages/DoctorSight-PacientProfile/index.jsx
+++ src/pages/DoctorSight-PacientProfile/index.jsx
@@ -1,37 +1,51 @@
 import React, { Component } from 'react'
 import backButton from "../../assets/images/go-back-arrow.png";
-import { BackButton } from "./style";
+import pacient_profile from "../../assets/images/pacient_profile.png";
+import { Header } from '../../components/layout/Headers/HeaderLogged/style';
+import { SectionTitle, SectionInfo, SectionInfo2, LayoutPageDoctorSightPacientProfile } from './style';
 
 export default function DoctorSightPacient() {
   return (
-    <>
+    <LayoutPageDoctorSightPacientProfile>
       {/* HeaderLoggedDoctor */}
+
+      <Header></Header>
 
       <SectionTitle>
         INFORMAÇÕES DE PERFIL
-        <button><img src="" alt="" /></button>
+        <div className='botao-voltar'><img src={backButton} alt="Botão Voltar" /></div>
+
       </SectionTitle>
 
       <SectionInfo>
-        <img src="" alt="" />
-        <label htmlFor="">Nome:</label>
-        <input type="text" />
-        <label htmlFor="">Nome:</label>
-        <input type="text" />
-        <label htmlFor="">Nome:</label>
-        <input type="text" />
-        <label htmlFor="">Nome:</label>
-        <input type="text" />
-        <label htmlFor="">Nome:</label>
-        <input type="text" />
-        <label htmlFor="">Nome:</label>
-        <input type="text" />
+        <div className='imgProfile'>
+          <img src={pacient_profile} alt="" />
+        </div>
+
+        <div className="infoProfile">
+          <div className="colA">
+            <label htmlFor="">Nome:</label>
+            <input type="text" readOnly/>
+            <label htmlFor="">Altura:</label>
+            <input type="text" readOnly/>
+            <label htmlFor="">Plano de Saúde: </label>
+            <input type="text" readOnly/>
+          </div>
+          <div className="colB">
+            <label htmlFor=""> Paciente desde:</label>
+            <input type="date" readOnly/>
+            <label htmlFor="">Gênero:</label>
+            <input type="text" readOnly/>
+            <label htmlFor="">Peso:</label>
+            <input type="text" readOnly/>
+          </div>
+        </div>
       </SectionInfo>
 
       <SectionInfo2>
-        
+
       </SectionInfo2>
-    </>
+    </LayoutPageDoctorSightPacientProfile>
   )
 
 }
--- src/pages/DoctorSight-PacientProfile/style.js
+++ src/pages/DoctorSight-PacientProfile/style.js
@@ -1,3 +1,92 @@
-export const BackButton = styled.button `
+import styled from "styled-components"
+
+
+export const LayoutPageDoctorSightPacientProfile = styled.section `
+  margin: 0 auto;
+  padding: 0;
+  box-sizing: border-box;
+
+  overflow: overlay;
+  
+  background-color: #057FD7;
+
+  width: 100%;
+  height: 100%;
+
+  font-size: 62.5%;
+  /* 1rem = 10px */
+`
+
+export const SectionTitle = styled.section `
+  
+  width : 117.2rem;
+  height: 7.4rem;
+
+  margin: 5.4rem auto 4.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  font-size: 3.6rem;
+  line-height: 4.4rem;
+  font-weight: 600;
+
+  color: #000000;
+
+  .botao-voltar {
+    border: none;
+    width: fit-content;
+    color: #FFFFFF;
+
+  }
+
+  .botao-voltar img {
+    width: 5rem;
+    height: 5rem;
+
+  }
+`
+
+export const SectionInfo = styled.section `
+  margin: 4.5rem auto 10rem;
+  padding: 0;
+
+  width: 117.2rem;
+  height: 40rem;
+  
+  display: flex;
+  align-items: center;
+  gap: 4.6rem;
+
+  border-block-color: #6F6F6F;
+
+ 
+
+  .infoProfile {
+    display: flex;
+    gap: 6.7rem;
+  }
+
+  .colA {
+    height: 25.4rem;
+    width: 25.4rem;
+
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .colB {
+    height: 25.4rem;
+    width: 25.4rem;
+    
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+`
+export const SectionInfo2 = styled.section `
+  
 
 `
